cfad489d8f07c3aa83f06c2a36cb4db1c67c4fdc
HuongNM2192 <nguyenminhhuong2192@gmail.com>
2019-06-13 04:59:10 +0700

    done tic-tac-toe game

--- src/index.js
+++ src/index.js
@@ -29,13 +29,16 @@ function Square(props){
 class Board extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { squares: Array(9).fill(null) };
+        this.state = { squares: Array(9).fill(null),isNext: true };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares: squares });
+        if(calculateWinner(squares) || squares[i]){
+            return;
+        }
+        squares[i] = this.state.isNext ? 'X' : 'O';
+        this.setState({ squares: squares, isNext: !this.state.isNext });
     }
 
     renderSquare(i) {
@@ -48,7 +51,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if(winner){
+            status = `Winner: ${winner}`;
+        }else{
+            status = `Next player: ${this.state.isNext ? 'X' : 'O'}`;
+        }
 
         return (
             <div>
@@ -95,3 +104,23 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
